9ca374b79071c7553057958a6f64aace5d8914a4
Ivan Barbosa Pinheiro <ivanpinheiro@outlook.com>
2025-02-26 02:40:46 -0300

    update import nuxt env

--- frontend/plugins/ghostApi.ts
+++ frontend/plugins/ghostApi.ts
@@ -4,9 +4,9 @@ export default defineNuxtPlugin(() => {
   const config = useRuntimeConfig();
 
   const api = new GhostContentApi({
-    url: config.public.GHOST_API_URL,
-    key: config.public.GHOST_API_KEY,
-    version: config.public.GHOST_API_VERSION,
+    url: config.public.ghostApiBase,
+    key: config.public.ghostApiKey,
+    version: config.public.ghostApiVersion,
   });
 
   return {
