d553bcb5fe86c0ce73949ccbc8c100826b084790
Ivan Barbosa Pinheiro <ivanpinheiro@outlook.com>
2025-04-27 20:34:13 -0300

    update backend stack

--- frontend/pages/backend-tech.vue
+++ frontend/pages/backend-tech.vue
@@ -6,50 +6,31 @@
         Back-end
       </h1>
       <p class="font-bold text-lg pl-12 pr-12">
-        O Blog Guaradata foi desenvolvido usando as tecnologias mais recentes e eficientes nos bastidores. Isso
-        significa que, por trás da aparência bonita que você vê, há um sistema sólido e rápido que cuida de tudo, desde
-        armazenar as informações até garantir que o site funcione sem problemas, mesmo quando muitas pessoas o visitam
-        ao mesmo tempo. Então, você pode navegar tranquilamente, sabendo que o Blog Guaradata está equipado com o que há
-        de melhor para tornar sua experiência a melhor possível.
+        O Blog Guaradata foi desenvolvido utilizando tecnologias robustas como Docker, Ghost CMS, MySQL e Nginx. Por trás da aparência bonita que você vê, existe um sistema sólido e rápido, responsável por armazenar informações e garantir que o site funcione perfeitamente, mesmo com muitos visitantes ao mesmo tempo. Assim, você pode navegar com tranquilidade, sabendo que o Blog Guaradata utiliza o que há de melhor para oferecer a melhor experiência possível.
       </p>
     </div>
     <div class="second-section w-full flex justify-center">
-      <div class="img p-5">
-        <NuxtLink to="https://nestjs.com/">
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nestjs/nestjs-original-wordmark.svg">
-        </NuxtLink>
-      </div>
-      <div class="img p-5">
-        <NuxtLink to="https://www.typescriptlang.org/">
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-plain.svg">
-        </NuxtLink>
-      </div>
-      <div class="img p-5">
-        <NuxtLink to="https://www.mongodb.com/">
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original-wordmark.svg">
-        </NuxtLink>
-      </div>
       <div class="img p-5">
         <NuxtLink to="https://www.docker.com/">
           <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/docker/docker-original.svg">
         </NuxtLink>
       </div>
       <div class="img p-5">
-        <NuxtLink to="https://mongoosejs.com/">
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongoose/mongoose-original-wordmark.svg">
+        <NuxtLink to="https://ghost.org/">
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/ghost/ghost-original-wordmark.svg">
+        </NuxtLink>
+      </div>
+      <div class="img p-5">
+        <NuxtLink to="https://nginx.org/">
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nginx/nginx-original.svg">
         </NuxtLink>
       </div>
     </div>
     <div class="second-section w-full flex justify-center">
       <div class="img p-5">
-        <NuxtLink to="https://www.prisma.io/">
-          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/prisma/prisma-original-wordmark.svg">
-        </NuxtLink>
-      </div>
-      <div class="img p-5">
-        <NuxtLink to="https://www.postgresql.org/">
+        <NuxtLink to="https://www.mysql.com/">
           <img
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original-wordmark.svg">
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-plain-wordmark.svg">
         </NuxtLink>
       </div>
     </div>
