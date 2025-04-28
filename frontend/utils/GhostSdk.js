417a68568652fc041a958b3b62de1af67d5cdb76
Ivan Barbosa Pinheiro <ivanpinheiro@outlook.com>
2025-04-28 02:24:47 -0300

    removed old components and added tags in axios request

--- frontend/utils/GhostSdk.js
+++ frontend/utils/GhostSdk.js
@@ -14,7 +14,7 @@ export async function getPost(postSlug) {
   const runtimeConfig = useRuntimeConfig();
   const apiUrl = `${runtimeConfig.public.ghostApiBase}/ghost/api/content/posts/slug`;
   try {
-    const { data } = await useFetch(`${apiUrl}/${postSlug}/?key=${runtimeConfig.public.ghostApiKey}`);
+    const { data } = await useFetch(`${apiUrl}/${postSlug}/?key=${runtimeConfig.public.ghostApiKey}&include=tags,authors`);
     return data.value;
   } catch (err) {
     console.error(`Erro ao buscar o post: ${postSlug}`, err);
